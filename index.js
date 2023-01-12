3d0e3f3b65433769008ec95fff932074f4d64a33
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2023-01-12 17:19:01 +0100

    blog backend server connected to db, ready to be tested with Render hosted db

--- index.js
+++ index.js
@@ -1,53 +1,56 @@
-// require('dotenv').config()
-// const { Sequelize, Model, DataTypes } = require('sequelize')
+require('dotenv').config()
+const { Sequelize, Model, DataTypes } = require('sequelize')
 const express = require('express')
 const app = express()
 
-// app.use(express.json())
+app.use(express.json())
 
-// const sequelize = new Sequelize(process.env.DATABASE_URL)
+const sequelize = new Sequelize(process.env.DATABASE_URL)
 
-// class Note extends Model { }
+class Blog extends Model { }
 
-// Note.init({
-//   id: {
-//     type: DataTypes.INTEGER,
-//     primaryKey: true,
-//     autoIncrement: true,
-//   },
-//   content: {
-//     type: DataTypes.TEXT,
-//     allowNull: false,
-//   },
-//   important: {
-//     type: DataTypes.BOOLEAN,
-//   },
-//   date: {
-//     type: DataTypes.DATE,
-//   }
-// }, {
-//   sequelize,
-//   underscored: true,
-//   timestamps: false,
-//   modelName: 'note'
-// })
-
-app.get('/api/blogs', async (req, res) => {
-  res.json({
-    message: 'hieperdepiep!',
-  })
-  // const notes = await Note.findAll()
-  // res.json(notes)
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  author: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  }
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
 })
 
-// app.post('/api/notes', async (req, res) => {
-//   try {
-//     const note = await Note.create(req.body)
-//     return res.json(note)
-//   } catch (error) {
-//     return res.status(400).json({ error })
-//   }
-// })
+app.get('/api/blogs', async (req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs)
+})
+
+app.post('/api/blogs', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body)
+    return res.json(blog)
+  } catch (error) {
+    return res.status(400).json({ error })
+  }
+})
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
