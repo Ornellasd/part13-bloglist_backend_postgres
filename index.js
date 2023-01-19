2f324677b6659fcc6513201ff003969eb39cd21e
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2023-01-19 13:55:10 +0100

    finishes 13.5 - init db function implemented; application restructured

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const { Sequelize, Model, DataTypes } = require('sequelize')
 const express = require('express')
 const app = express()
 
 const { PORT } = require('./util/config')
+const { connectToDatabase } = require('./util/db')
 
 const blogsRouter = require('./controllers/blogs')
 
@@ -10,6 +10,13 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+const start = async () => {
+  await connectToDatabase()
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+}
+
+start()
+
+
