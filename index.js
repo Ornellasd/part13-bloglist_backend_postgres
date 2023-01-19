066995e4612e9654a55082814b85fda8a31d90b1
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2023-01-19 12:55:09 +0100

    break out controller

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const { Sequelize, Model, DataTypes } = require('sequelize')
 const express = require('express')
 const app = express()
 
+const blogsRouter = require('./controllers/blogs')
+
 app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
 
 const sequelize = new Sequelize(process.env.DATABASE_URL)
 
@@ -38,28 +42,7 @@ Blog.init({
   modelName: 'blog'
 })
 
-app.get('/api/blogs', async (req, res) => {
-  const blogs = await Blog.findAll()
-  res.json(blogs)
-})
 
-app.post('/api/blogs', async (req, res) => {
-  try {
-    const blog = await Blog.create(req.body)
-    return res.json(blog)
-  } catch (error) {
-    return res.status(400).json({ error })
-  }
-})
-
-app.delete('/api/blogs/:id', async (req, res) => {
-  const blog = await Blog.findByPk(req.params.id)
-  if (blog) {
-    await blog.destroy();
-  } else {
-    res.status(404).end()
-  }
-});
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
