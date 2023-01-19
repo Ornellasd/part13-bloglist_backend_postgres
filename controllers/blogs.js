48330f05703df3b3a2e663456ee1451895b8e2ac
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2023-01-19 13:45:01 +0100

    break out blog model

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const Blog = require('../models/blog')
 
 router.get('/', async (req, res) => {
   const blogs = await Blog.findAll()
--- index.js
+++ index.js
@@ -10,39 +10,6 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 
-const sequelize = new Sequelize(process.env.DATABASE_URL)
-
-class Blog extends Model { }
-
-Blog.init({
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  author: {
-    type: DataTypes.TEXT,
-    allowNull: true
-  },
-  url: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-  title: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-  likes: {
-    type: DataTypes.INTEGER,
-    defaultValue: 0,
-  }
-}, {
-  sequelize,
-  underscored: true,
-  timestamps: false,
-  modelName: 'blog'
-})
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
